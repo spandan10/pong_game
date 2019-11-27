f491503d69d9742e1618057a36540c961d584630
spandan10 <41497309+spandan10@users.noreply.github.com>
2019-11-27 06:58:53 +0530

    Update 6 - Score mechanism added
    Adding a scoring mechanism

--- pong_game.py
+++ pong_game.py
@@ -7,6 +7,10 @@ wn.bgcolor("black") #Classic pong game color
 wn.setup(width=800, height=600)
 wn.tracer(0) #Stops the window from updating. Speed up our games a bit
 
+# Score
+score_a = 0
+score_b = 0
+
 #Paddle A
 paddle_a = turtle.Turtle()
 paddle_a.speed(0) #Speed of the animation. Sets the speed of the maximum possible speed
@@ -35,6 +39,15 @@ ball.goto(0,0)
 
 ball.dx = 0.2 # ball movement in x-axis by 2 pixels
 ball.dy = -0.2 # ball movement in y-axis by 2 pixels
+
+# Pen
+pen = turtle.Turtle()
+pen.speed(0) # Animation speed
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0,260)
+pen.write("Player A: 0  Player B: 0", align="center", font=("Courier", 24, "normal"))
 
 #Function
 def paddle_a_up():
@@ -84,11 +97,17 @@ while True:
     if ball.xcor() > 390:
         ball.goto(0,0) #Since, the ball will go out of the game. It should come to the centre
         ball.dx *= -1
-    
+        score_a += 1
+        pen.clear()
+        pen.write("Player A: {}  Player B: {}".format(score_a,score_b), align="center", font=("Courier", 24, "normal"))
+        
     if ball.xcor() < -390:
         ball.goto(0,0) #Since, the ball will go out of the game. It should come to the centre
         ball.dx *= -1
-        
+        score_b += 1
+        pen.clear()
+        pen.write("Player A: {}  Player B: {}".format(score_a,score_b), align="center", font=("Courier", 24, "normal"))        
+    
     # Paddle and ball collision
     if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor() + 40 and ball.ycor() > paddle_b.ycor() -40):
         ball.setx(340)
